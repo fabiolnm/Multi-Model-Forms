110a855a78d462e0b8b122ac1d09361cb3c824a3
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-08-05 00:52:44 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiModelForms::Application.config.secret_token = '6fbec490ff5aff589dafefc6ea9e746396e023814b8faac4fe95959ad2f57a2d57f5e2a635ae011497e4119103bee98bbf7572213e966480ae73f64576a5719d'
