69d34eff45a64340ffb1b346326c0ecc2a382f70
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-08-05 03:04:34 -0300

    Nested permissions

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -1,8 +1,10 @@
 class UserAccount < ActiveRecord::Base
-  attr_accessible :password, :username
+  attr_accessible :password, :username, :permissions_attributes
 
   validates_presence_of :username, :password, :person
   belongs_to :person
-  has_many :permissions
+  has_many :permissions, inverse_of: :user_account
+
+  accepts_nested_attributes_for :permissions, allow_destroy: true
 end
 
